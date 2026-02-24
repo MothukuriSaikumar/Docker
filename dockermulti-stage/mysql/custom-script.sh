9b194fdc1d0b09fe5d2acda6cb83af6191ad5058
Saikumar <mskumar.a64@gmail.com>
2026-02-24 10:18:12 +0530

    chnages

--- dockermulti-stage/mysql/custom-script.sh
+++ dockermulti-stage/mysql/custom-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# This is a custom script that will be executed when the MySQL container starts.
+
 if [ -f /tmp/mysql-root-password.txt ]; then
     PASSWORD=$(cat /tmp/mysql-root-password.txt)
     echo  "Accessed MySQL root password"
 else
-    echo " MySQL root password not found "
+    echo " MySQL root password file not found "
     exit 1
 
 
